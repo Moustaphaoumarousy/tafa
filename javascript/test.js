8ed3c5849f63b476164932c96f2dcabb3b8a9b70
Moustaphaoumarousy <142666798+Moustaphaoumarousy@users.noreply.github.com>
2023-10-23 09:01:40 +0000

    Add files via upload

---
+++ javascript/test.js
@@ -0,0 +1,14 @@
+// alert('hi, ceci est un alert');      //programme d'instruction
+// document.write("Hello world")
+
+// console.log("probleme")
+// console.error("probleme")
+
+const age = prompt('Bonjour, chére utilisateur quel est votre age: ')
+if (age<=13) {
+    console.log("je vous suggere de regarder l'animer")
+} else if (13< age >18) {
+    console.log("jeune addo je vous propose le film 1")
+} else {
+    console.log("le film 2 vous etez proposée ")
+}
